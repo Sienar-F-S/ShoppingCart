b781342c797bbf9b9a71d7ecba488339e915fc91
sienar <rosekula@protonmail.com>
2019-04-17 17:48:21 +0100

    Cart with scanner
    Added cart class with scanner function

---
+++ Cart.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+// Cart to track customers shopping list
+
+namespace ShoppingCart {
+	class Cart {
+		private Dictionary<string, int> _scannedItems = new Dictionary<string, int>();
+
+		// Item scanner adds shopping to cart. Can take multiple items with delimeter ,
+		public void ScanItems(string items) {
+			string tmp;
+
+			foreach (string item in items.Split(",")) {
+				tmp = item.Trim().ToLower();
+
+				_scannedItems[tmp] = _scannedItems.ContainsKey(tmp) ? _scannedItems[tmp] + 1 : 1;
+			}
+		}
+	}
+}
--- Program.cs
+++ Program.cs
@@ -1,12 +1,26 @@
 ﻿using System;
+using System.Collections.Generic;
+
+// Simple Shopping Cart feature
 
 namespace ShoppingCart
 {
     class Program
     {
+
+		private Cart cart;
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-        }
+			Console.WriteLine($"Args {args.Length}");
+			Cart cart = new Cart();
+
+			if (args.Length > 0) {
+				cart.ScanItems(args[0]);
+			} else {
+				cart.ScanItems("Apple, Apple, apple, orange, apple");
+			}
+		}
+
     }
 }

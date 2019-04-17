0c5c6ce10b8ba1c11813951ab885e8434e71037c
sienar <rosekula@protonmail.com>
2019-04-17 18:31:23 +0100

    Stage-1-Added_price_to_stock
    Changed stock to dict to contain the price for an item.

--- Cart.cs
+++ Cart.cs
@@ -4,7 +4,10 @@ using System.Collections.Generic;
 // Cart to track customers shopping list
 namespace ShoppingCart {
 	static class Stock {
-		public static string[] allStock = { "apple", "orange" };
+		public static Dictionary<string, decimal> allStock = new Dictionary<string, decimal> {
+			{"apple", 0.60m},
+			{"orange", 0.25m}
+		};
 	}
 
 	class Cart {
@@ -15,12 +18,13 @@ namespace ShoppingCart {
 			foreach (string item in items.Split(",")) {
 				AddItemToCart(item.Trim().ToLower());
 			}
+
 			PrintCart();
 		}
 
 		// Adds an available item to the cart
 		private void AddItemToCart(string item) {
-			if (!Array.Exists(Stock.allStock, x => x == item))
+			if (!Stock.allStock.ContainsKey(item))
 				return;
 
 			_scannedItems[item] = _scannedItems.ContainsKey(item) ? _scannedItems[item] + 1 : 1;
@@ -28,7 +32,7 @@ namespace ShoppingCart {
 
 		public void PrintCart() {
 			foreach (var item in _scannedItems.Keys) {
-				Console.WriteLine($"{item}: {_scannedItems[item]}");
+				Console.WriteLine($"{item}: {_scannedItems[item] }. Price: {_scannedItems[item] * Stock.allStock[item]}");
 			}
 		}
 	}
